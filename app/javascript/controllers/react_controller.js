e2ba31ff811cf62ed892f1ac9c7bb0d287feada6
Jose Antonio Riquelme <jariquelme@gmail.com>
2023-03-07 14:51:58 +0100

    fix warning

--- app/javascript/controllers/react_controller.js
+++ app/javascript/controllers/react_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import React from "react"
 import { createRoot } from 'react-dom/client';
 
-import SurveyForm from "../components/Survey_form"
+import SurveyForm from "../components/Survey_Form"
 
 export default class extends Controller {
   connect() {
